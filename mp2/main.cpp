bd8783cf5f10c82765176f5fc7b23bfeb4d35893
seanrobenalt <srob0722@gmail.com>
2019-06-12 12:59:02 -0400

    add comments for the professor

--- mp2/main.cpp
+++ mp2/main.cpp
@@ -3,39 +3,55 @@
 using namespace std;
 
 int main() {
+  // read the file
   ifstream applicants("input.txt");
 
+  // setup variable to store each line in
   string applicant;
-  int total_expected_applicants = 14;
 
+  // counter for looping through each line
+  int total_input_lines = 14;
+
+  // setup variables to store applicant info in
   string school;
   float gpa;
   int math;
   int verbal;
   string alumnus;
 
+  // setup counters for number/type of applicants
   int liberal_arts_students_accepted = 0;
   int music_students_accepted = 0;
   int total_actual_applicants = 0;
 
-  for (int i = 1; i <= total_expected_applicants; i++) {
-    total_actual_applicants += 1;
+  cout << "Acceptance to College by Sean Robenalt" << endl << endl;
 
+  // loop through each line of file
+  for (int i = 1; i <= total_input_lines; i++) {
+
+    // increment number of applicants by 1
+    total_actual_applicants++;
+
+    // setup variables to use for output
     string rejection_string = "Rejected - ";
     bool accept = true;
 
+    // get a line of file, store it in applicant variable
     getline(applicants, applicant);
 
+    // parse the line and store info
     school = applicant.front();
     gpa = strtof((applicant.substr(2, 3)).c_str(), 0);
     math = stoi(applicant.substr(6, 3));
     verbal = stoi(applicant.substr(10, 3));
     alumnus = applicant.back();
 
+    // print out applicant info
     cout << "Applicant #: " << i << endl;
     cout << "School = " << school << " GPA = " << gpa << " math = " << math;
     cout << " verbal = " << verbal << " alumnus = " << alumnus << endl;
 
+    // perform logic to determine whether accepted or rejected
     if (school == "L") {
 
       cout << "Applying to " << "Liberal Arts" << endl;
@@ -106,6 +122,7 @@ int main() {
     }
   }
 
+  // print out final numbers
   cout << "There were " << total_actual_applicants << " applicants in the file." << endl;
   cout << "There were " << liberal_arts_students_accepted << " acceptances to Liberal Arts." << endl;
   cout << "There were " << music_students_accepted << " acceptances to Music." << endl;
